7c801f36945c6ec7e636b9db7e2cd33a00cecf86
Nnamdi Okoroafor <nnamdiokoroafor567@gmail.com>
2021-09-06 11:26:59 +0100

    git steps to create a new repo and push to repo

---
+++ index.js
@@ -0,0 +1,10 @@
+const git_steps=[
+    'steps to push code to git hub',
+    1='create a repo on github',
+    2.='copy repo url',
+    git_clone_url='use git clone with repo url to pull repo to local',
+    git_add= 'to add files to local machine',
+    git_commit='to commit changes',
+    git_push='to push changes to repo'
+
+]
